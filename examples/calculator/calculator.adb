b6f300a6e65b6d220b61814692074ea194d1cb27
ofrisberg <ollefrisberg@hotmail.com>
2018-06-08 16:26:07 +0200

    Before starting with nested functions/structs/procedures

--- examples/calculator/calculator.adb
+++ examples/calculator/calculator.adb
@@ -1,6 +1,7 @@
 with Ada.Text_IO, Ada.Integer_Text_IO;
 use Ada.Text_IO, Ada.Integer_Text_IO;
-
+--konvertera .ads till .h, namespace i header-filen (ex. Ada)
+--use -> using i c++
 procedure Calculator is
 	n1: Integer := 2;
 	n2: Integer := 3;
@@ -21,8 +22,30 @@ procedure Calculator is
 		return z;
 	end Sub;
 	
+	type Constant_type is
+		record
+			name : String(1..2);
+			value : Float;
+		end record;
+		
+	pi : Constant_type;
+	
+	procedure Print_numbers(nr1: Integer; nr2: Integer) is
+	begin
+		Put(nr1);
+		Put(nr2);
+	end Print_numbers;
+	
+	procedure Print_hello is
+	begin
+		Put_Line("Hello");
+	end Print_hello;
+	
 begin
 
+	pi.name := "Pi";
+	pi.value := 3.14;
+	
 	n3 := Add(n1,n2);
 	n4 := Sub(n3,n2);
 	Put(n4);
--- src/prog.py
+++ src/prog.py
@@ -1,32 +1,31 @@
 import argparse
+import ntpath
 import xml.etree.ElementTree as ET
+from cppfile import CppFile
 
 def printChilds(node):
 	for child in node:
 		print child.tag, child.attrib
 
+
 argparser = argparse.ArgumentParser()
 argparser.add_argument('filename')
 args = argparser.parse_args()
 
-
-tree = ET.parse(args.filename)
+tree = ET.parse((args.filename).strip("\r"))
 
 node = tree.getroot()
 node = node.find('unit_declaration_q')
 node = node.find('procedure_body_declaration')
 node = node.find('body_declarative_items_ql')
 
-for functionNode in node.findall('function_body_declaration'):
-	functionName = functionNode.find('names_ql').find('defining_identifier').get('def_name')
-	print "Funktion '"+functionName+"'"
-	for paramNode in functionNode.find('parameter_profile_ql').findall('parameter_specification'):
-		param = {}
-		param['name'] = paramNode.find('names_ql').find('defining_identifier').get('def_name')
-		param['type'] = paramNode.find('object_declaration_view_q').find('identifier').get('ref_name')
-		print " - Param: '"+param['name'] + "'. Typ: '" + param['type']+"'"
-	output = functionNode.find('result_profile_q').find('identifier').get('ref_name')
-	print " - Returtyp: '"+output+"'"
+cpp_file = ntpath.basename(args.filename).split('.')
+cpp_file = ''.join(cpp_file[:len(cpp_file)-2])
+cpp_file = cpp_file + ".cpp"
+cpp = CppFile(cpp_file)
 
-
-
+cpp.extractStructs(node)
+cpp.extractFunctions(node)
+#cpp.extractProcedures(node)
+for function in cpp.functions: cpp.printFunction(function)
+cpp.write()
